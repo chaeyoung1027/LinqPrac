f93fb80627e72dff2f975af570e142e8851c443a
Chaeyoung Lim <rnldyal1027@naver.com>
2023-08-07 12:06:39 +0900

    Linq 없는 방식으로 2의 배수 구하기

--- LinqPrac/Program.cs
+++ LinqPrac/Program.cs
@@ -10,6 +10,18 @@ namespace LinqPrac
     {
         static void Main(string[] args)
         {
+            List<int> input = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> output = new List<int>();
+
+            //Linq없이 2의 배수 구하기
+            foreach (int i in input)
+            {
+                if(i%2==0) output.Add(i);
+            }
+            foreach(var i in output)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
